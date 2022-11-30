d8277011e9ff877cb2f47c80c4de3d11adc7dce3
Rivaldo Jr <rivaldojr@gmail.com>
2022-11-30 17:10:27 -0300

    01

---
+++ src/componentes/AbCard/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const AbCard = () => {
+    return (
+        <div>
+            <div>
+                Sobre o livro:
+                <div>
+                    Liderança em Design
+                    <div>
+                        Habilidades de gestão para alavancar sua carreira
+                        Por: Vitor Zanini
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -16,3 +16,4 @@ export const Thing: FC<Props> = ({ children }) => {
 
 
 export * from './componentes/AbBotao';
+export * from './componentes/AbCard';
